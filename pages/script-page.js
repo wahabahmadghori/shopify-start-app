6459ee0726d7672c93c7e9ba223ffda53693c22a
wahabahmadghori <wahabahmadghori@gmail.com>
2020-07-18 11:34:13 +0500

    polaris Create

--- pages/script-page.js
+++ pages/script-page.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import gql from 'graphql-tag'
+import React from "react";
+import gql from "graphql-tag";
 import { AiFillDelete } from "react-icons/ai";
-import {useQuery,useMutation} from '@apollo/react-hooks'
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { Page } from "@shopify/polaris";
 
 const CREATE_SCRIPT_TAG = gql`
-mutation scriptTagCreate($input: ScriptTagInput!) {
+  mutation scriptTagCreate($input: ScriptTagInput!) {
     scriptTagCreate(input: $input) {
       scriptTag {
         id
@@ -15,20 +16,20 @@ mutation scriptTagCreate($input: ScriptTagInput!) {
       }
     }
   }
-`
+`;
 const Query_SCRIPTTAGS = gql`
-query{
-  scriptTags(first:5){
-    edges{
-      node {
-        id
-        src
-        displayScope
+  query {
+    scriptTags(first: 5) {
+      edges {
+        node {
+          id
+          src
+          displayScope
+        }
       }
     }
   }
-}
-`
+`;
 const DELETE_SCRIPTTAGS = gql(`
 mutation scriptTagDelete($id: ID!) {
   scriptTagDelete(id: $id) {
@@ -39,55 +40,48 @@ mutation scriptTagDelete($id: ID!) {
     }
   }
 }
-`)
+`);
 
 const ScriptPage = () => {
-  const {loading,error,data} = useQuery(Query_SCRIPTTAGS)
-  const [createScripts] = useMutation(CREATE_SCRIPT_TAG)
-  const [deleteScripts] = useMutation(DELETE_SCRIPTTAGS)
+  const { loading, error, data } = useQuery(Query_SCRIPTTAGS);
+  const [createScripts] = useMutation(CREATE_SCRIPT_TAG);
+  const [deleteScripts] = useMutation(DELETE_SCRIPTTAGS);
 
-  if(loading) return <div>loading....</div>
-  if(error) return <div>{error.message}</div>
-  console.log(data)
-    return (
-        <div>
-            <button type="submit" 
-            onClick={()=>{
-              createScripts({
-                variables:{
-                  input:{
-                    src:"https://shopifyapp01.herokuapp.com/test-script.js",
-                    displayScope: "ALL"
-                  }
-                },
-                refetchQueries:[{query:Query_SCRIPTTAGS}]
-              })
-            }}>
-              Creat Script Tag
-            </button>
-            <h2>
-               Script Tags List:
-            </h2>
-            {data.scriptTags.edges.map(edge=>{
-              return (
-              <p key={edge.node.id}>
-                {`id:\t${edge.node.id} src:\t${edge.node.src}`}
-                <AiFillDelete 
-                style={{fontSize:"2rem",cursor:"pointer",color:"#ff2222"}}
-                onClick={()=>{
-                  deleteScripts({
-                    variables:{
-                      id:edge.node.id
-                    },
-                    refetchQueries:[{query:Query_SCRIPTTAGS}]
-                  })
-                }}/>
-                </p>)
-            })}
-            
-            
-        </div>
-    )
-}
+  if (loading) return <div>loading....</div>;
+  if (error) return <div>{error.message}</div>;
+  console.log(data);
+  return (
+    <Page>
+      <Layout>
+        <Layout.Section>
+          <Card title="Script Page" sectioned>
+            <p>Create Script Tag</p>
+          </Card>
+        </Layout.Section>
+        <Layout.Section>
+          <Card sectioned>
+          <Button 
+          size="slim" 
+          primary={true}
+          onClick={()=>{
+            createScripts({
+              variables:{
+                input:{
+                  src:"https://shopifyapp01.herokuapp.com/test-script.js",
+                  displayScope: "ALL"
+                }
+              },
+              refetchQueries:[{query:Query_SCRIPTTAGS}]
+            })
+          }}>
+          >
+            Create Tag
+          </Button>
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+};
 
-export default ScriptPage
+export default ScriptPage;
