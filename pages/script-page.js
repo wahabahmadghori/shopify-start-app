29b1fb358148652fe92876c605ea4b7dd2d44f85
wahabahmadghori <wahabahmadghori@gmail.com>
2020-07-18 08:41:27 +0500

    trash-icon

--- pages/script-page.js
+++ pages/script-page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import gql from 'graphql-tag'
+import { AiFillDelete } from "react-icons/ai";
 import {useQuery,useMutation} from '@apollo/react-hooks'
 
 const CREATE_SCRIPT_TAG = gql`
@@ -28,6 +29,17 @@ query{
   }
 }
 `
+const DELETE_SCRIPTTAGS = gql(`
+mutation scriptTagDelete($id: ID!) {
+  scriptTagDelete(id: $id) {
+    deletedScriptTagId
+    userErrors {
+      field
+      message
+    }
+  }
+}
+`)
 
 const ScriptPage = () => {
   const {loading,error,data} = useQuery(Query_SCRIPTTAGS)
@@ -55,7 +67,11 @@ const ScriptPage = () => {
                Script Tags List:
             </h2>
             {data.scriptTags.edges.map(edge=>{
-              return <p key={edge.node.id}>{`id:${edge.node.id} src:${edge.node.src}`}</p>
+              return (
+              <p key={edge.node.id}>
+                {`id:  ${edge.node.id} src:  ${edge.node.src}`}
+                <AiFillDelete/>
+                </p>)
             })}
             
             
