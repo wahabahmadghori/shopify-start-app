5d3453f786bd8aca6cd198bd6669148f90c6ab07
wahabahmadghori <wahabahmadghori@gmail.com>
2020-07-21 15:45:20 +0500

    pass-data-body

--- public/test-script.js
+++ public/test-script.js
@@ -6,6 +6,7 @@ const makeHeader = (data)=>{
 fetch('https://cors-anywhere.herokuapp.com/https://shopifyapp01.herokuapp.com/api/products?shop=mstarterapp.myshopify.com')
 .then(res=>res.json()).then(data=>{
     console.log(data.body)
+    makeHeader(data.body)
 }).catch(error=>{
     console.log(error)
 })
