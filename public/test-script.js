91160c1a49de43f96d73263d007f8d4c9b337a27
wahabahmadghori <wahabahmadghori@gmail.com>
2020-07-21 09:25:44 +0500

    test-script-link

--- public/test-script.js
+++ public/test-script.js
@@ -1,2 +1,5 @@
 console.log('This is comming from script tag api')
 $('.site-header').prepend('<div style="background:red;">Free Shipping</div>')
+
+fetch('https://shopifyapp01.herokuapp.com/api/products?shop=mstarterapp.myshopify.com')
+.then(data=>console.log(data.data)).catch(error=>console.log(error))
