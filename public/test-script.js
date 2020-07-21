256bd7cd6963427e9b1ee92ceb1063b532349121
wahabahmadghori <wahabahmadghori@gmail.com>
2020-07-21 14:59:00 +0500

    cors-anywhere

--- public/test-script.js
+++ public/test-script.js
@@ -2,7 +2,7 @@ console.log('This is comming from script tag api')
 $('.site-header').prepend('<div style="background:red;">Free Shipping</div>')
 
 console.log("Hello")
-fetch('https://shopifyapp01.herokuapp.com/api/products?shop=mstarterapp.myshopify.com')
+fetch('https://cors-anywhere.herokuapp.com/https://shopifyapp01.herokuapp.com/api/products?shop=mstarterapp.myshopify.com')
 .then(res=>res.json()).then(data=>{
     console.log(data.data)
 }).catch(error=>{
