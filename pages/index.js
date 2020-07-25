3e3cbb7331102ddb84bbbc35b66c1dec100f04f1
wahabahmadghori <wahabahmadghori@gmail.com>
2020-07-25 14:55:27 +0500

    method

--- pages/index.js
+++ pages/index.js
@@ -18,16 +18,14 @@ function Index() {
     selectedResources.map((product) => makeApiCall(product));
   }
   async function makeApiCall(products) {
-    const url = '/api/products';
-    console.log(products)
-    axios
-      .post(url,products)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    axios({
+      method: 'post',
+      url: '/api/products',
+      data: {
+        firstName: 'Fred',
+        lastName: 'Flintstone'
+      }
+    });
   }
 
   return (
