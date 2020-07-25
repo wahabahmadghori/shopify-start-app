02e84d0aac2e95aa560a48fa8a111c4892561aae
wahabahmadghori <wahabahmadghori@gmail.com>
2020-07-25 10:22:45 +0500

    abc

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,67 @@
-import React, {useState} from 'react'
-import {EmptyState,Page,Layout} from '@shopify/polaris'
-import {ResourcePicker,TitleBar} from '@shopify/app-bridge-react'
-import store from 'store-js'
-import ProductList from '../components/ProductList'
-import axios from 'axios'
+import React, { useState } from "react";
+import { EmptyState, Page, Layout } from "@shopify/polaris";
+import { ResourcePicker, TitleBar } from "@shopify/app-bridge-react";
+import store from "store-js";
+import ProductList from "../components/ProductList";
+import axios from "axios";
 
-function Index(){
-  const [modal, setModal] = useState({open:false})
-  const emptyState = !store.get('ids')
+function Index() {
+  const [modal, setModal] = useState({ open: false });
+  const emptyState = !store.get("ids");
 
-  function handleSelection(resources){
-    const idsFromRosourses = resources.selection.map(product=>product.id)
-    setModal({open:false})
-    store.set('ids',idsFromRosourses)
-    console.log(store.get('ids'))
-    const selectedResources = resources.selection
-    selectedResources.map(product=>makeApiCall(product))
+  function handleSelection(resources) {
+    const idsFromRosourses = resources.selection.map((product) => product.id);
+    setModal({ open: false });
+    store.set("ids", idsFromRosourses);
+    console.log(store.get("ids"));
+    const selectedResources = resources.selection;
+    selectedResources.map((product) => makeApiCall(product));
   }
-  async function makeApiCall(products){
-    const url = '/api/products'
-    axios.post(url,products).then((result)=>{
-      console.log(result)
-    }).catch((error)=>{
-      console.log(error)
-    })
+  async function makeApiCall(products) {
+    const url = "/api/products";
+    axios
+      .post(url, products)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-    return (
-      <Page>
-        <TitleBar
+  return (
+    <Page>
+      <TitleBar
         primaryAction={{
-          content:"Select Products",
-          onAction:()=>setModal({open:true})
+          content: "Select Products",
+          onAction: () => setModal({ open: true }),
         }}
-        >
-          
-        </TitleBar>
-        <ResourcePicker
-        resourceType='Product'
+      ></TitleBar>
+      <ResourcePicker
+        resourceType="Product"
         showVariants={false}
         open={modal.open}
-        onCancel={()=>setModal({open:false})}
-        onSelection={(resources)=>handleSelection(resources)}
-        />
-        {emptyState?
+        onCancel={() => setModal({ open: false })}
+        onSelection={(resources) => handleSelection(resources)}
+      />
+      {emptyState ? (
         <Layout>
           <EmptyState
             heading="Manage your inventory transfers"
-            action={{ 
+            action={{
               content: "Select Products",
-              onAction:()=>setModal({open:true})
+              onAction: () => setModal({ open: true }),
             }}
             image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
-            
           >
             <p>Select Products</p>
           </EmptyState>
-        </Layout>:<ProductList/>}
-      </Page>
-    );
+        </Layout>
+      ) : (
+        <ProductList />
+      )}
+    </Page>
+  );
 }
 
-export default Index
+export default Index;
